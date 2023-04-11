a1479ec495f41ca9864c8924440f0b5f90813152
gjpineda <gjpineda@live.com>
2023-04-11 11:43:17 -0700

    added knex link

--- server/knexfile.js
+++ server/knexfile.js
@@ -9,8 +9,7 @@ if (process.env.NODE_ENV !== "production") {
 module.exports = {
   client: "mysql2",
   connection: {
-    host: "/",
-    // This links to the .env
+    host: process.env.DB_LOCAL_HOST,
     database: process.env.DB_LOCAL_DBNAME,
     user: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASSWORD,
