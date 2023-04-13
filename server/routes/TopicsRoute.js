1606400521653218b13dc31913d786210749011f
gjpineda <gjpineda@live.com>
2023-04-13 14:04:28 -0700

    disabled sorting func for demo

--- server/routes/TopicsRoute.js
+++ server/routes/TopicsRoute.js
@@ -9,25 +9,25 @@ const { queues } = require("../utilities/memory_data");
 const router = express.Router();
 
 //--------------------------------------------------------------
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
+// function shuffle(array) {
+//   let currentIndex = array.length,
+//     randomIndex;
 
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+//   // While there remain elements to shuffle.
+//   while (currentIndex != 0) {
+//     // Pick a remaining element.
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex--;
 
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
+//     // And swap it with the current element.
+//     [array[currentIndex], array[randomIndex]] = [
+//       array[randomIndex],
+//       array[currentIndex],
+//     ];
+//   }
 
-  return array;
-}
+//   return array;
+// }
 
 //-----TINDER SORTING ALGORITHM----------------------------------------------------
 
@@ -129,10 +129,11 @@ router.post("/api/topics/:topic_id/disagree", (req, res) => {
 router.get("/api/topics", async (req, res) => {
   try {
     const topics = await knex("topics");
-    // shuffles the topics
-    const shuffleTopics = shuffle(topics);
+    // // shuffles the topics
+    // const shuffleTopics = shuffle(topics);
 
-    res.send(shuffleTopics);
+    // res.send(shuffleTopics);
+    res.send(topics);
   } catch (error) {
     res.status(500).send(`Something went wrong`);
   }
